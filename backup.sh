8b6950ed790ca048cb4a768e5b0d3dadd7091eff
madih <mahmoudmadih150@gamil.com>
2026-06-16 13:19:11 +0300

    make some fixes

--- backup.sh
+++ backup.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-#varibles
+
+# variables
 SOURCE_DIR="/root/backup"
 LOG_FILE="backup.log"
 REMOTE_HOST="madih@192.168.0.103"
 REMOTE_DIR="/root/"
-#fanuctions to perfom the buckup
-prefome_backup(){
-    rsync -avz "$SOURCE_DIR" "$REMOTE_HOST":"REMOTE_DIR" > "LOG_FILE" 2>&1
-    if[ $? -eq 0 ];then
-        echo "buckup Sucssesful  $(date)" >> "$LOG_FILE" 
-    else
-        echo "buckup field  $(date)" >> "$LOG_FILE" 
-    fi
 
+# function to perform backup
+perform_backup(){
+
+    rsync -avz "$SOURCE_DIR" "$REMOTE_HOST:$REMOTE_DIR" > "$LOG_FILE" 2>&1
+
+    if [ $? -eq 0 ]; then
+        echo "backup successful $(date)" >> "$LOG_FILE"
+    else
+        echo "backup failed $(date)" >> "$LOG_FILE"
+    fi
 }
-#run the backup
-prefome_backup
+
+# run backup
+perform_backup
